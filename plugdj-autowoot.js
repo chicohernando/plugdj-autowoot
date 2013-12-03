be4a7c4b0f733752f0b006257d9090d9a9e8c42c
Eric Sanders <eric.sanders@wellspringworldwide.com>
2013-12-03 10:23:08 -0600

    Working on adding Google Analytics

--- plugdj-autowoot.js
+++ plugdj-autowoot.js
@@ -2,6 +2,27 @@
 var chicohernando = {};
 
 /**
+ * Adds custom namespaced GA key to the page
+ */
+chicohernando.setupGoogleAnalytics = function() {
+	_gaq.push(['chicohernando._setAccount', 'UA-46047123-1']);
+};
+
+/**
+ * Track the page that this is being used on
+ */
+chicohernando.trackPageView = function() {
+	_gaq.push(['chicohernando._trackPageview', '/autowoot/' + location.host +  location.pathname]);
+};
+
+/**
+ * Track number of woots that happen
+ */
+chicohernando.trackEvent = function(category, action) {
+	_gaq.push(['chicohernando._trackEvent', category, action]);
+};
+
+/**
  * Returns a random integer for use as a timeout
  * between a new user getting on deck and wooting
  */
@@ -14,7 +35,8 @@ chicohernando.getRandomTimeout = function() {
  * a click event.
  */
 chicohernando.woot = function() {
-  jQuery('#woot').trigger('click');  
+  jQuery('#woot').trigger('click');
+  chicohernando.trackEvent('auto', 'woot');
 };
 
 /**
@@ -28,4 +50,6 @@ API.bind(API.DJ_ADVANCE, function(obj) {
 });
 
 //initial script load triggers a woot
+chicohernando.setupGoogleAnalytics();
+chicohernando.trackPageView();
 chicohernando.woot();
